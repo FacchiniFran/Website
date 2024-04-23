e8067e0749c6b5491beec7588397c7853b8c10b1
FacchiniFran <facchinifn@gmail.com>
2024-04-23 19:40:37 -0300

    Header apto Tablet y Mobile

---
+++ js/script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    var checkboxes = document.querySelectorAll(".cArrow");
+    checkboxes.forEach(function(checkbox) {
+        checkbox.checked = false;
+    });
+
+    var burger = document.querySelector('.mBurger');
+    if (burger) {
+        burger.checked = false;
+    }
+};
