c8b5cab26dd5ab66b8b9c24f5716d32d13b603eb
FacchiniFran <facchinifn@gmail.com>
2024-06-04 21:20:18 -0300

    ajustes script

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 window.onload = function() {
-    var checkboxes = document.querySelectorAll(".cArrow");
+    let checkboxes = document.querySelectorAll(".cArrow");
     checkboxes.forEach(function(checkbox) {
         checkbox.checked = false;
     });
 
-    var burger = document.querySelector(".mBurger");
+    let burger = document.querySelector(".mBurger");
     if (burger) {
         burger.checked = false;
     }
